f80a6c1e584c258edb7919275f564cc13d609693
Troy Robinson <troy@lionsrock.com>
2025-03-26 21:33:42 -0600

    Add default title to Page layout

--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -3,7 +3,7 @@ import * as Avatar from '@radix-ui/react-avatar';
 import { FlexCol, FlexRow } from '../utils.jsx';
 import Navigation from './Navigation.jsx';
 
-export const PageLayout = ({ children, title, style }) => {
+export const PageLayout = ({ children, title = 'Welcome', style }) => {
   return (
     <FlexCol
       style={{
@@ -59,7 +59,7 @@ export const PageLayout = ({ children, title, style }) => {
       
       <div style={{ height: '1px', backgroundColor: '#e0e0e0', margin: '0 0 20px 0' }} />
       
-      <Navigation />
+      <Navigation style={{}} />
       
       {title && <h2 style={{ fontSize: '1.8rem', padding: '20px 0' }}>{title}</h2>}
       
